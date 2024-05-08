070facb84991ab13f94b1e1d44afd247f1c5ad75
sgyhu <js1harvee10@hotmail.com>
2024-05-08 11:52:03 +0700

    test task

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function squareDigits(num) {
-  return parseInt(
-    num
-      .toString()
-      .split("")
-      .map((digit) => digit ** 2)
-      .join(""),
-  );
+function isSymmetric(root) {
+  if (!root) return true;
+  return isMirror(root.left, root.right);
+  function isMirror(left, right) {
+    if (!left && !right) return true;
+    if (!left || !right || left.val !== right.val) return false;
+    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+  }
 }
