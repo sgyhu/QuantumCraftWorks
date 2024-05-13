45138a8a0ee295aaaca88e79c769e069c6e4910b
sgyhu <js1harvee10@hotmail.com>
2024-05-13 20:43:38 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-function fibonacci(n) {
-  if (n <= 1) return n;
-  return fibonacci(n - 1) + fibonacci(n - 2);
+function reverseBetween(head, m, n) {
+  if (!head || m === n) return head;
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let prev = dummy;
+  for (let i = 0; i < m - 1; i++) prev = prev.next;
+  let start = prev.next;
+  let then = start.next;
+  for (let i = 0; i < n - m; i++) {
+    start.next = then.next;
+    then.next = prev.next;
+    prev.next = then;
+    then = start.next;
+  }
+  return dummy.next;
 }
